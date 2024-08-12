2096039742ce1771948d1eb4b91b1ef851ca5bf4
Cristian Rojas <criisuser07@gmail.com>
2024-08-11 21:04:02 -0600

    feature: add first Autorization annotation

--- src/Project4.Api/Controllers/MarcasAutosController.cs
+++ src/Project4.Api/Controllers/MarcasAutosController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Mvc;
 using Project4.Api.Extensions;
 using Project4.Application.Endpoints.MarcasAutos.Queries;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Project4.Api.Controllers
 {
@@ -21,6 +22,7 @@ namespace Project4.Api.Controllers
         //Creamos nuestro punto de entrada para enviarlo a la clase handler por medio del mediator 
         [HttpGet]
         [Route("/marcas")]
+        [Authorize]
         public async Task<ActionResult> GetMarcasAutosAsync()
         {
             var request = new MarcasAutosQuery(); 
